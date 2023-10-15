6144631c6119b50cee7394eeabde0a0b32ca056c
Dương Tiến Đạt <datduong7302@gmail.com>
2023-10-15 20:44:43 +0700

    add permission model

---
+++ database/migrations/2023_10_14_031120_create_permissions_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('permissions', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name');
+        });
+        Schema::create('permission_role', function (Blueprint $table) {
+            $table->integer('permission_id');
+            $table->integer('role_id');
+        });
+        Schema::table('permission_role', function (Blueprint $table) {
+
+            $table->foreign('permission_id')->references('id')->on('permissions')->onDelete('cascade');
+            $table->foreign('role_id')->references('id')->on('roles')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('permission_role');
+        Schema::dropIfExists('permissions');
+    }
+};
